c318fe9cd589a51de1229daba1b8f1184d62a51f
fahao <asd@qq.com>
2017-11-12 21:55:36 +0800

    init main

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "struct.hpp"
+using namespace std;
+
+void print_ui();
+
+int main(void) {
+    family obj;
+    while(1) {
+        print_ui(obj);
+
+    }
+    return 0;
+}
+
+void print_ui(family& obj) {
+    cout << "# 家谱管理系统 #" << endl;
+    obj.traverse()
+}
